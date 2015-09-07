5ad3ce0ff110bcc9d347b3689bd354e0977e75cf
w3cp <jannat.books@gmail.com>
2015-09-07 12:36:41 +0000

    adding 1_1.c

---
+++ c/cpbook.subeen.com/ch01/1_1.c
@@ -0,0 +1,22 @@
+/*
+// program : 1.1
+
+// 2015-09-07-06-47
+
+// code  example cpbook.subeen.com
+
+// MD ANWAR HOSSAIN 
+// http://w3cp.blogspot.com
+
+// https://github.com/w3cp/
+// https://github.com/w3cp/coding/
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}

ef466dbb250c495dee2906fdb6c5f3680db16644
Anwar Hossain <jannat.books@gmail.com>
2019-09-25 09:50:56 +0600

    Create sqrt.c

---
+++ math/square_root/sqrt.c
@@ -0,0 +1,31 @@
+// to find out sqrt
+// https://github.com/w3cp
+
+#include <stdio.h>
+
+double mysqrt(double num); // to return the sqrt of its parameter
+
+int  main() {
+    double x;
+    scanf("%lf", &x);
+    printf("%0.2lf\n", mysqrt(x) );
+    return 0;    
+} // end main
+
+// returns the sqrt of its parameter
+double mysqrt(double number) {
+    double low, high, mid;
+    
+    low = 0.0;
+    high = number;
+    
+    while (high - low > 0.000000001) {
+        mid = (low + high) / 2;
+        if (mid * mid > number)
+            high = mid;
+        else
+            low = mid;
+    }
+    
+    return mid;    
+} // end mysqrt
